fd2d0f24d1cbea1215217bae80c36ffd775b66cc
DBovee27 <d.bovee@sintlucasedu.nl>
2025-01-30 22:44:53 +0100

    Parallax scrolling test1

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,4 +7,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
 const test = document.getElementById("Ap");
 test.innerText = "0dsadas"
 
-    }
+let scrollPosition = 0;
+let targetPosition = 0;
+let speed = 0.2;
+
+window.addEventListener('scroll', function(){
+    targetPosition = window.scrollY;
+   })
+    
+function smoothParallax() {
+    scrollPosition = (targetPosition - scrollPosition) * speed;
+    document.body.style.backgroundPositionY = scrollPosition * 5 + "px";
+
+    requestAnimationFrame(smoothParallax);
+    
+}   
+smoothParallax()
+
+}
