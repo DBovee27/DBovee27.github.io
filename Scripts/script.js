8bb6b86a9952965e71d8adb19c473358c43f793c
DBovee27 <d.bovee@sintlucasedu.nl>
2025-01-30 18:09:46 +0100

    1

---
+++ Scripts/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    loaded();
+})
+
+    function loaded() {
+
+const test = document.getElementById("Ap");
+test.innerText = "0dsadas"
+
+    }
